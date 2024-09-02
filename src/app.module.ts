5fe03c6531729e55df02115f5d7a3b9e0c48016a
Awais Ahmed Malik <mawais_malik@hotmail.com>
2024-09-02 03:56:54 -0400

    Added MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TodoModule,
+    MongooseModule.forRoot(process.env.MONGODB_URL),
   ],
   controllers: [AppController],
   providers: [AppService],
